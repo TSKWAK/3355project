598206267a13fd4b97640bae95c2279b80afc2ab
wlsgu <wlsgu@LAPTOP-IC8H3VHE>
2021-07-12 12:15:10 +0900

    jinhyun2번째테스트

---
+++ 3355project/src/main/java/com/project/mybatisDAO/BoardDao.java
@@ -0,0 +1,17 @@
+package com.project.mybatisDAO;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.project.entity.Board;
+
+@Mapper
+public interface BoardDao {
+	List<Board> getList(String boardlist, int page);
+	
+	int getCount(String boardlist);
+
+	List<Board> getDetail(int boardid);
+}
