0881fab67dd689ae69f35ebba8f1a0cfcf461f77
TSKWAK <TSKWAK@TSKWAK>
2021-07-12 15:55:41 +0900

    회원가입 간단하게 추가

--- 3355vue/src/store/index.js
+++ 3355vue/src/store/index.js
@@ -77,6 +77,17 @@ export default new Vuex.Store({
             console.log("getDetail error: ", payload)
           });
     },
+    test({ commit }, payload){
+            axios.post('https://jsonplaceholder.typicode.com/tests',$store.state.account)
+            .then((res)=>{
+                console.log(res.data)
+            })
+            .catch((err)=>{
+                console.log(err)
+            }).finally(()=>{
+                //ddd
+            });
+    },
 
   },
 
--- src/main/java/com/project/mybatisDAO/BoardDao.java
+++ src/main/java/com/project/mybatisDAO/BoardDao.java
@@ -3,7 +3,6 @@ package com.project.mybatisDAO;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import com.project.entity.Board;
 
