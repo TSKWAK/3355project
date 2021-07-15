74396b0c02786dd8fe65475a5931cf46132a5c86
TSKWAK <TSKWAK@TSKWAK>
2021-07-15 11:56:24 +0900

    회원가입 완성됨 1. 비밀번호는 sha1로 암호화했음 (복호화불가능) 2. 이메일 인증은 임시이메일 사이트로 확인했음 https://www.moakt.com/ko
    3. 회원가입 안에서만 동작해서 vuex는 안썼음 나중에 써야하면 옮김

--- src/main/java/com/project/controller/AccountController.java
+++ src/main/java/com/project/controller/AccountController.java
@@ -1,17 +1,30 @@
 package com.project.controller;
 
+import java.util.Random;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.entity.Users;
 import com.project.mybatisDAO.AccountDao;
 
-@Controller
+@RestController
 @RequestMapping("/api/user")
 public class AccountController {
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	@Autowired
 	AccountDao dao;
@@ -21,9 +34,58 @@ public class AccountController {
 		System.out.println(users);
 		
 		dao.addUserDao(users.getUserId(),users.getUserPw(),users.getUserEmail());
+	}
+	
+	@GetMapping("/checkEmail")
+	public String checkEmail(String userEmailAddr) {
+		Random random = new Random();
+		String key="";
 		
+		System.out.println(userEmailAddr);
 		
+		for(int i=0; i<3; i++) {
+			int index = random.nextInt(25)+65;
+			key+=(char)index;
+		}
+		int numIndex = random.nextInt(9999)+1000;
 		
+		key += numIndex;
+		
+		MimeMessage msg = javaMailSender.createMimeMessage();
+		
+		try {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(msg,true,"UTF-8");
+			messageHelper.setSubject("3355 인증번호");
+	        messageHelper.setText("인증번호 : " + key);
+	        messageHelper.setTo(userEmailAddr);
+            msg.setRecipients(MimeMessage.RecipientType.TO , InternetAddress.parse(userEmailAddr));
+            javaMailSender.send(msg);
+	        
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		
+		/*
+		 * SimpleMailMessage message = new SimpleMailMessage();
+		 * message.setTo(userEmailAddr);
+		 * 
+		 * 
+		 * 
+		 * message.setSubject("3355 인증번호"); message.setText("인증번호 : " + key);
+		 * javaMailSender.send(message);
+		 */
+		
+		return key;
+	}
+	
+	
+	@GetMapping("/checkUserId")
+	public int check_UserId(String userId) {
+		System.out.println(userId);
+		int cnt = dao.viewUserDao(userId);
+		System.out.println(cnt);
+		
+		return cnt;
 	}
 	
 	
--- src/main/java/com/project/mybatisDAO/AccountDao.java
+++ src/main/java/com/project/mybatisDAO/AccountDao.java
@@ -9,7 +9,7 @@ import com.project.entity.Users;
 @Mapper
 public interface AccountDao {
 	public List<Users> userList();
-	public Users viewUserDao(String userId);
+	public int viewUserDao(String userId);
 	public int addUserDao(String userId, String UserPw, String UserEmail);
 	public int deleteUserDao(String userId);
 	
